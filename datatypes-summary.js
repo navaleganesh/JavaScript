6355bb4a4ef9ad1b4ae449edc692b6fb6bfd8d13
navaleganesh <navale.ganesh1@gmail.com>
2025-01-03 23:28:40 +0530

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,46 @@
+/* Data type */
+
+// Primitive (Call by value)
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol,
+//           BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+const bigNumber = 3156454896n
+
+
+// Reference (Non primitive - Call by reference)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj", "doga"]
+
+let myObj = {
+    name: "ganesh",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof bigNumber); // BigInt
+console.log(typeof outsideTemp); // Object
+console.log(typeof anotherId); // Symbol
+console.log(typeof userEmail); // undefined
+console.log(typeof myFunction); // function
+console.log(typeof heros); // object
+
+// Is javascript dynamically type or statically type language
+// It is a dynamically language.
