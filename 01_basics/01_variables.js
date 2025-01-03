7c9f56c4fc823a3dbb66cadc8c80ac2e618c6719
navaleganesh <navale.ganesh1@gmail.com>
2025-01-03 20:51:47 +0530

    Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ganesh@google.com"
+var accoutPassword = "12345"
+accountCity = "Pune"
+let accountState
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
