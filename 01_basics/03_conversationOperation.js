e53237511b8cafd33aeca97def39ff01bc97da7b
navaleganesh <navale.ganesh1@gmail.com>
2025-01-03 23:29:42 +0530

    change in location of file

--- 01_basics/03_conversationOperation.js
+++ 01_basics/03_conversationOperation.js
@@ -42,7 +42,57 @@
 // "hitesh" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************* Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true); //bad habit
+// console.log(+"");  //bad habit
+
+// let num1, num2, num3 
+// num1 = num2 = num3 = 2 + 2 //bad habit
+
+let gameCounter = 100
+// gameCounter++ // postfix 
+// ++gameCounter // prefix
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;   // return the value before the increment
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a; // return the value after the increment
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
