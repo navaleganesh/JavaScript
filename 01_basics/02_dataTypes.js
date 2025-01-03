89e67033440aad0a767c330e0772f1ccbe66a71f
navaleganesh <navale.ganesh1@gmail.com>
2025-01-03 21:09:58 +0530

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // teat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+    console.log("Ganesh");
+
+    let name = "ganesh"
+    let age = 18
+    let isLoggedIn = true  // false
+
+
+    /*Primitive data type*/
+    // number => 2 to power of 53
+    // bigint 
+    // string => ""
+    // boolean => true/false
+    // null => Standalone value (Representation of empty value)
+    // undefined => abhi value assign nahi hui
+    // symbol => Unique find karne ke liye use hota hai
+
+
+
+    // object
+    
+console.log(typeof age); // o/p: number
+console.log(typeof null); // o/p: object
+console.log(typeof undefined); // o/p : undefined
