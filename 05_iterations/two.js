ca2874cb23cf4ff689c40e0895b78b6d163ce204
navaleganesh <navale.ganesh1@gmail.com>
2025-01-10 18:49:12 +0530

    Iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,30 @@
+// while
+
+// let index = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index++
+// }
+
+// let  myArray= ["flash","batman","superman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+
+
+
+// let score = 1
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++    
+// } while (score <=10 );
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`); //o/p: Score is 11
+    score++    
+} while (score <=10 );
